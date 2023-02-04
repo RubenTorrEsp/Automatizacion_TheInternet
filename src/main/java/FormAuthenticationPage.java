c2da4fdf93320e4d4dea2b129cfaacf0043878b0
Ruben <ruben.mte@gmail.com>
2023-02-04 19:21:50 +0100

    Form Authentication tests created

--- src/main/java/FormAuthenticationPage.java
+++ src/main/java/FormAuthenticationPage.java
@@ -1,9 +1,27 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class FormAuthenticationPage extends _BasePage {
 
     public FormAuthenticationPage(WebDriver driver) {
         super(driver);
     }
+
+    String correctUser = "tomsmith";
+    String wrongUser = "will smith";
+    String correctPass = "SuperSecretPassword!";
+    String wrongPass = "1234";
+
+    //String message = driver.findElement(By.id("flash")).toString();
+
+    By input = By.cssSelector("input");
+    By buton = By.cssSelector("button");
+
+    List<WebElement> inputs = driver.findElements(input);
+    WebElement userName = inputs.get(0);
+    WebElement password = inputs.get(1);
 
 }
--- src/test/java/FormAuthenticationTests.java
+++ src/test/java/FormAuthenticationTests.java
@@ -1,5 +1,7 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class FormAuthenticationTests extends _Common {
 
     FormAuthenticationPage fap;
@@ -10,8 +12,51 @@ public class FormAuthenticationTests extends _Common {
     }
 
     @Test
-    public void test() {
+    public void correct_login_works() {
         setUpPage();
+
+        fap.userName.sendKeys(fap.correctUser);
+        fap.password.sendKeys(fap.correctPass);
+        base.click(fap.buton);
+
+        //System.out.println(fap.message);
+
+        String expectedURL = "secure";
+        boolean actualURL = driver.getCurrentUrl().endsWith(expectedURL);
+
+        assertTrue(actualURL);
+    }
+
+    @Test
+    public void incorrect_user_works() {
+        setUpPage();
+
+        fap.userName.sendKeys(fap.wrongUser);
+        fap.password.sendKeys(fap.correctPass);
+        base.click(fap.buton);
+
+        //System.out.println(fap.message);
+
+        String expectedURL = "login";
+        boolean actualURL = driver.getCurrentUrl().endsWith(expectedURL);
+
+        assertTrue(actualURL);
+    }
+
+    @Test
+    public void incorrect_password_works() {
+        setUpPage();
+
+        fap.userName.sendKeys(fap.correctUser);
+        fap.password.sendKeys(fap.wrongPass);
+        base.click(fap.buton);
+
+        //System.out.println(fap.message);
+
+        String expectedURL = "login";
+        boolean actualURL = driver.getCurrentUrl().endsWith(expectedURL);
+
+        assertTrue(actualURL);
     }
 
 }
